76823c0e3cf0a3cfc02f84db4ef3517d33ad2336
tathagatgaikwad22 <122632025+tathagatgaikwad22@users.noreply.github.com>
2023-01-13 22:31:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+    define('TITLE',"Home | Home Page");
+    include 'includes/header.php';
+?>
+
+
+
+<div id="philosophy">
+    <hr>
+    <br><br>
+    <h1>Login Authentication With Profile</h1>
+    <br><br>
+    <p>Use this page as a homepage to show content to users even they are not logged it. The functioning can be changed
+    so that the user cannot access any other page other than a login page unless he is signed it. This can be achieved 
+    by replacing this page with a login page and checking if the user in logged on every other page and redirecting to 
+    login page if he is not</p>
+    
+    <br><br><br><br>
+    <hr>
+</div>
+
+<?php 
+    include 'includes/footer.php';
+?>
